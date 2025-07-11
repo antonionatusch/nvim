eef0e74a6c932efbae02397d8e76c7a54a171047
Antonio Natusch <anthony2004951@gmail.com>
2025-07-11 11:13:51 -0400

    feat(plugins): add json-to-types plugin
    Esto me permite, dado un archivo .json (que usualmente va a contener la
    respuesta de una petición GET a un endpoint), generar una interfaz de
    typescript (o de cualquier lenguaje) para poder trabajar con una mejor
    estructura de datos. Este plugin
    https://github.com/Redoxahmii/json-to-types.nvim tiene las instrucciones
    para poder agregar atajos para hacer esta conversión hacia cualquier
    tipo de archivo. Eso sí, es necesario instalar quicktype (
    https://github.com/glideapps/quicktype ) de forma global antes. `npm
    install -g quicktype`

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -163,5 +163,22 @@ local plugins = {
       require("custom.configs.copilot") -- Carga la configuración desde configs/copilot.lua
     end,
   },
+  {
+    "Redoxahmii/json-to-types.nvim",
+    build = "sh install.sh npm", -- Replace `npm` with your preferred package manager (e.g., yarn, pnpm).
+    ft = "json",
+    keys = {
+      {
+        "<leader>cU",
+        "<CMD>ConvertJSONtoLang typescript<CR>",
+        desc = "Convert JSON to TS",
+      },
+      {
+        "<leader>ct",
+        "<CMD>ConvertJSONtoLangBuffer typescript<CR>",
+        desc = "Convert JSON to TS Buffer",
+      },
+    },
+  }
 }
 return plugins
