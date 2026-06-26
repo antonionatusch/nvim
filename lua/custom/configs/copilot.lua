a2dc2acac43332a6f701edbd3c6a0b8b79c99a4c
Antonio Natusch <anthony2004951@gmail.com>
2026-06-26 16:10:18 -0400

    chore(copilot): fix node version
    Should solve crashes

--- lua/custom/configs/copilot.lua
+++ lua/custom/configs/copilot.lua
@@ -1,2 +1,4 @@
-vim.g.copilot_no_tab_map = true -- Desactiva el mapeo por defecto de la tecla Tab
+vim.g.copilot_node_command = "/home/tusch/.nvm/versions/node/v20.20.2/bin/node"
+
+vim.g.copilot_no_tab_map = true
 vim.api.nvim_set_keymap("i", "<C-g>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
